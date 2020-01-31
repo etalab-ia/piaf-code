b30d09662ed5f02ec0d7ddc467874e066069b3a9
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-01-31 15:29:22 +0100

    added licence specification to najukis code

--- wiki-preparation/WikipediaPagerank/SqlReader.java
+++ wiki-preparation/WikipediaPagerank/SqlReader.java
@@ -1,11 +1,29 @@
-/* 
+/*
  * Computing Wikipedia's internal PageRanks
- * 
- * Copyright (c) 2017 Project Nayuki
- * All rights reserved. Contact Nayuki for licensing.
- * https://www.nayuki.io/page/computing-wikipedias-internal-pageranks
- */
 
+ * Copyright (c) 2017 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/computing-wikipedias-internal-pageranks
+ *
+ * Adapted by Etalab in the context of the PIAF project
+ * https://piaf.etalab.studio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ *
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
